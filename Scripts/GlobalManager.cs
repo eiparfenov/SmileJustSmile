d7e352a84701a0cb6d6574c22584cadec99905ae
eiparfenov <62956146+eiparfenov@users.noreply.github.com>
2022-03-27 18:53:37 +0300

    Add main menu UI manager

--- Scripts/GlobalManager.cs
+++ Scripts/GlobalManager.cs
@@ -11,43 +11,26 @@ public class GlobalManager : MonoBehaviour
 
     [SerializeField] private GameObject gameUI;
     [SerializeField] private GameObject deadUI;
-
-    [SerializeField] private Button restartButton;
-    [SerializeField] private Button menuButton;
-    [SerializeField] private Text reasonOfDeath;
-
+    
     private GameObject _currentLevel;
     private void Start()
     {
         _currentLevel = Instantiate(level);
         GlobalEventsManager.OnPlayerDead.AddListener(DieHandler);
-        restartButton.onClick.AddListener(LevelRestartListener);
-        menuButton.onClick.AddListener(GoToMenuListener);
     }
 
     private void DieHandler(DieType type)
     {
-        if (type == DieType.Mask)
-            reasonOfDeath.text = "Вы слишком долго носили маску";
-        else
-            reasonOfDeath.text = "Вас заметили";
-        
-        gameUI.SetActive(false);
-        deadUI.SetActive(true);
-        
         Time.timeScale = 0f;
         GlobalEventsManager.RemoveAllListeners();
     }
    
     private void LevelRestartListener()
     {
-        print("Restart");
         Destroy(_currentLevel);
         _currentLevel = Instantiate(level);
         GlobalEventsManager.OnPlayerDead.AddListener(DieHandler);
         Time.timeScale = 1f;
-        gameUI.SetActive(true);
-        deadUI.SetActive(false);
         FindObjectOfType<Joystick>().ResetHeadPosition();
     }
 
--- Scripts/UI/MainMenuUI.cs
+++ Scripts/UI/MainMenuUI.cs
@@ -1,9 +1,137 @@
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
 
 namespace UI
 {
     public class MainMenuUI : MonoBehaviour
     {
+        [Header("GameUI")]
+        [SerializeField] private GameObject gameUI;
         
+        [Header("MainUI")]
+        [SerializeField] private GameObject mainUI;
+        [SerializeField] private Button startButton;
+        [SerializeField] private Button levelsButton;
+        [SerializeField] private Button settingsButton;
+        [SerializeField] private Button quitButton;
+        [Header("LevelsUI")]
+        [SerializeField] private GameObject levelsUI;
+        [SerializeField] private Button backLevelsButton;
+        
+        [Header("SettingsUI")]
+        [SerializeField] private GameObject settingsUI;
+        [SerializeField] private Button backSettingsButton;
+        
+        [Header("DeadUI")]
+        [SerializeField] private GameObject deadUI;
+        [SerializeField] private Button restartLevelButton;
+        [SerializeField] private Button goToMainDeadButton;
+        [SerializeField] private Text deadText;
+        
+        [Header("WinUI")]
+        [SerializeField] private GameObject winUI;
+        [SerializeField] private Button nextLevelButton;
+        [SerializeField] private Button goToMainAliveButton;
+
+        public UnityEvent onStartGame = new UnityEvent();
+        public UnityEvent onRestartLevel = new UnityEvent();
+        public UnityEvent onNextLevel = new UnityEvent();
+        public UnityEvent onReturnToMainMenu = new UnityEvent();
+
+        private void Start()
+        {
+            startButton.onClick.AddListener(StartButtonPressHandler);
+            levelsButton.onClick.AddListener(LevelsButtonPressHandler);
+            settingsButton.onClick.AddListener(SettingsButtonPressHandler);
+            quitButton.onClick.AddListener(QuitButtonPressHandler);
+            backLevelsButton.onClick.AddListener(BackButtonPressHandler);
+            backSettingsButton.onClick.AddListener(BackButtonPressHandler);
+            restartLevelButton.onClick.AddListener(RestartLevelButtonPressHandler);
+            nextLevelButton.onClick.AddListener(NextLevelButtonPressHandler);
+            goToMainDeadButton.onClick.AddListener(ReturnToMainMenuButtonHandler);
+            goToMainAliveButton.onClick.AddListener(ReturnToMainMenuButtonHandler);
+        }
+        public void ShowLose(DieType type)
+        {
+            if (type == DieType.Killed)
+                deadText.text = "Смерть";
+            else
+                deadText.text = "Обнаружение";
+            
+            gameUI.SetActive(false);
+            deadUI.SetActive(true);
+        }
+        public void ShowWin()
+        {
+            gameUI.SetActive(false);
+            winUI.SetActive(true);
+        }
+        private void GoToMainMenu()
+        {
+            mainUI.SetActive(true);
+            gameUI.SetActive(false);
+            levelsUI.SetActive(false);
+            settingsUI.SetActive(false);
+            deadUI.SetActive(false);
+            winUI.SetActive(false);
+        }
+        private void GoToGameUI()
+        {
+            mainUI.SetActive(false);
+            gameUI.SetActive(true);
+            levelsUI.SetActive(false);
+            settingsUI.SetActive(false);
+            deadUI.SetActive(false);
+            winUI.SetActive(false);
+        }
+        
+        private void StartButtonPressHandler()
+        {
+            onStartGame.Invoke();
+            GoToGameUI();
+        }
+
+        private void LevelsButtonPressHandler()
+        {
+            mainUI.SetActive(false);
+            gameUI.SetActive(false);
+            levelsUI.SetActive(true);
+            settingsUI.SetActive(false);
+            deadUI.SetActive(false);
+            winUI.SetActive(false);
+        }
+        private void SettingsButtonPressHandler()
+        {
+            mainUI.SetActive(false);
+            gameUI.SetActive(false);
+            levelsUI.SetActive(false);
+            settingsUI.SetActive(true);
+            deadUI.SetActive(false);
+            winUI.SetActive(false);
+        }
+        private void BackButtonPressHandler()
+        {
+            GoToMainMenu();
+        }
+        private void QuitButtonPressHandler()
+        {
+            Application.Quit();
+        }
+
+        private void RestartLevelButtonPressHandler()
+        {
+            onRestartLevel.Invoke();
+        }
+
+        private void NextLevelButtonPressHandler()
+        {
+            onNextLevel.Invoke();
+        }
+
+        private void ReturnToMainMenuButtonHandler()
+        {
+            onReturnToMainMenu.Invoke();
+        }
     }
 }
